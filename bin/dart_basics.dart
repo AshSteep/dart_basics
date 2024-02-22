e1f26bd5640f81ed75f4613faae5b343c48018da
AshSteep <ashinsteephan2024a@mca.ajce.in>
2024-02-22 14:36:21 +0530

    First commit

---
+++ bin/dart_basics.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main() {
+  // print("Hello world");
+  // var a = stdin.readLineSync();
+  // print('$a');
+
+  // print("enter 2 numbers");
+  // var inp1 = stdin.readLineSync();
+  // var inp2 = stdin.readLineSync();
+
+  // var num1 = int.parse(inp1!);
+  // var num2 = int.parse(inp2!);
+
+  // print("sum = ${num1 + num2}");
+  
+  
+}
